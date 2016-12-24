b598461bebed79c3490ba58f3eb58074d13f1895
Your Name <josh@lospi.net>
2016-12-24 12:09:20 -0600

    Adding Profile tests

--- tests/serialization_test.cpp
+++ tests/serialization_test.cpp
@@ -9,11 +9,37 @@ class SerializationTest : public ::testing::Test {
         game.player1 = "player1";
         game.player2 = "player2";
         game.status = Game::Status::PLAYER1_SETTING_UP;
+
+        profile.id = "BCDE-2345";
+        profile.name = "player name";
+        profile.password = "plaintext password";
     }
 protected:
     Serializer serializer;
     Game game;
+    Profile profile;
 };
+
+TEST_F(SerializationTest, serializes_profile_id) {
+    auto serialized = serializer.from(profile);
+    auto deserialized = serializer.to_profile(serialized);
+
+    ASSERT_EQ(profile.id, deserialized.id);
+}
+
+TEST_F(SerializationTest, serializes_profile_name) {
+    auto serialized = serializer.from(profile);
+    auto deserialized = serializer.to_profile(serialized);
+
+    ASSERT_EQ(profile.name, deserialized.name);
+}
+
+TEST_F(SerializationTest, serializes_profile_password) {
+    auto serialized = serializer.from(profile);
+    auto deserialized = serializer.to_profile(serialized);
+
+    ASSERT_EQ(profile.password, deserialized.password);
+}
 
 TEST_F(SerializationTest, serializes_game_id) {
     auto serialized = serializer.from(game);
